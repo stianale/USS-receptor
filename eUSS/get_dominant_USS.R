19ae2c564fd294e2adab683900fa4ca600a0dda2
stianale <stianale@oslomet.no>
2025-05-23 13:47:55 +0200

    updated euss rscript

--- eUSS/get_dominant_USS.R
+++ eUSS/get_dominant_USS.R
@@ -6,3 +6,15 @@ df <- read.delim("Traits_file_all_orthofinder_with_genome_size.txt", sep = '\t',
 
 df <- df %>% mutate(Dominant = case_when(Hin_USS > Apl_USS ~ "Hin-USS",
                                          Apl_USS > Hin_USS ~ "Apl-USS"))
+
+hin_list <- df %>% filter(Dominant == "Hin-USS")
+
+apl_list <- df %>% filter(Dominant == "Apl-USS")
+
+hin_list <- hin_list %>% select(Genome)
+
+apl_list <- apl_list %>% select(Genome)
+
+write.table(hin_list, file = "hin_list.txt", col.names = FALSE, row.names = FALSE, quote = FALSE, sep = '\t')
+
+write.table(apl_list, file = "apl_list.txt", col.names = FALSE, row.names = FALSE, quote = FALSE, sep = '\t')
